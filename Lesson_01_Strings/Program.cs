c6a0a89f28a45b03af6094e18218091e03d24684
диан <@->
2022-02-01 16:36:11 +0200

    Add project files.

---
+++ Lesson_01_Strings/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Lesson_01_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task05_ReplaceWords.RunTheProgram();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Principles_Of_Programming_Hometasks/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Principles_Of_Programming_Hometasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Here are the hometasks");
+        }
+    }
+}
